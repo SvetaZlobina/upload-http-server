3b090ff65a76f5d50d024726b1bc5fc5e1ff8f4d
svetazlobina <svetlanazlobina97@gmail.com>
2018-11-18 13:33:45 +0300

    Realize upload method with rq

---
+++ process.py
@@ -0,0 +1,6 @@
+import time
+
+
+def process_images(index):
+    print('Making job')
+    time.sleep(2)
--- server.py
+++ server.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from flask import Flask, request, Response
+from redis import Redis
+from rq import Queue
+
+from process import process_images
+
+INPUT_DIR = 'images/input/'
+STYLE_DIR = 'images/style/'
 
 app = Flask(__name__)
+q = Queue(connection=Redis(), default_timeout=3600)
+
+index_count = 1
 
 
-@app.route('/')
-def hello():
-    return 'Hello!'
+@app.route('/process', methods=['POST'])
+def receive_order():
+    input_img = request.files['input']
+    style_img = request.files['style']
+    global index_count  # TODO: make unique index validation
+    input_img.save(INPUT_DIR+'input'+str(index_count)+'.png')
+    style_img.save(STYLE_DIR+'style'+str(index_count)+'.png')
+    index_count += 1
+
+    r = q.enqueue(process_images, 1)
+
+    return Response(r.id, status=201)
